9735fa13a7e4229b29ffdf4a1a388d6e4c371767
Kadar Tibor Manasse <kadar-tibor.manasse@hpe.com>
2017-07-14 11:52:26 +0300

    Added jersey

---
+++ src/main/java/com/example/rest/DefectResource.java
@@ -0,0 +1,17 @@
+package com.example.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/defects")
+public class DefectResource {
+
+    @GET
+    @Path("test")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String test() {
+        return "Test  def ---";
+    }
+}
