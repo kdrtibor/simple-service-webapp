e5cd3ab5bfd04aa98e974a77f97863d1ada71dee
Kadar Tibor Manasse <kadar-tibor.manasse@hpe.com>
2017-07-17 15:24:28 +0300

    created testing module

---
+++ simple-service-webapp-tests/src/test/java/com/example/TestTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestTest {
+
+    @Test
+    public void testPrintMessage() {
+        System.out.println("Inside testPrintMessage()");
+        Assert.assertTrue(true);
+    }
+
+}
