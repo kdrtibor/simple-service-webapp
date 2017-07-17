682f9c022e17a15dc5559a983b70af5f48219967
Kadar Tibor Manasse <kadar-tibor.manasse@hpe.com>
2017-07-17 15:12:49 +0300

    added a new module for tests

---
+++ simple-service-webapp-tests/src/main/java/com/example/MainClass.java
@@ -0,0 +1,10 @@
+package com.example;
+
+/**
+ * Created by manasse on 7/17/2017.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
