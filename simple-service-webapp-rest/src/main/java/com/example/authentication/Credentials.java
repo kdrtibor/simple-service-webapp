9cc61de74b892b67f6ed5ee83c0d31d91b3b8f45
Kadar Tibor Manasse <kadar-tibor.manasse@hpe.com>
2017-07-18 13:34:22 +0300

    Formatting

--- simple-service-webapp-rest/src/main/java/com/example/authentication/Credentials.java
+++ simple-service-webapp-rest/src/main/java/com/example/authentication/Credentials.java
@@ -1,8 +1,14 @@
 package com.example.authentication;
 
 public class Credentials {
+
     private String username;
     private String password;
+
+    public Credentials(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
     public String getUsername() {
         return username;
@@ -12,18 +18,6 @@ public class Credentials {
         this.username = username;
     }
 
-    public Credentials(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-                "username:" + username + "," +
-                "password:" + password +
-                '}';
-    }
 
     public String getPassword() {
         return password;
@@ -32,4 +26,10 @@ public class Credentials {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "{" + "username:" + username + "," + "password len:" + password.length() + '}';
+    }
+
 }
